10ca3e380852bee6efd58aba462c129bc2da3834
shjang1025 <shjang1025@gmail.com>
2024-02-20 11:31:24 -0800

    define part of SPIRE

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user.is_password?(password)
+            user
+        else 
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bc = BCrypt::Password.new(password_digest)
+        bc.is_password?(password)s
+    end
+
+    def reset_session_token!
+
+    end
+
+    def ensure_session_token
+        session_token ||= SecureRandom.urlsafe_base64
+    end
+
+end
