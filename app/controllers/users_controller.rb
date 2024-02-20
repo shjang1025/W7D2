f8b9b34c70b73b69fa84cc26a3c0ff53157ae33a
shjang1025 <shjang1025@gmail.com>
2024-02-20 13:13:33 -0800

    create users controller and the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    #User Interface section
+    def new
+        render :new
+    end
+    #API section
+    def create
+        user = User.new(user_params)
+        if user.save
+            log_in(user)
+            redirect_to user_url(user.id)
+        else
+            render :new
+        end
+    end
+
+
+    #helpers
+    private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
 end
